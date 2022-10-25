052b40d11c2e0ca1cddb9960b44b7d5f6fc9d4b9
Catalin Maeschi <catalin.maeschi23@gmail.com>
2022-10-25 16:48:05 +0300

    Adding Resizable component

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CodeCell from './components/code-cell';
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-    return( 
+  return (
     <div>
       <CodeCell />
     </div>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
